7f43eee4f88e71006ca4ea525aa41e49d2572e8b
JosePortillo <jose.portillo.samayoa@gmail.com>
2024-03-12 12:20:00 -0600

    feat: :sparkles:Adding Bodas's page v1

--- src/WeddingApp.tsx
+++ src/WeddingApp.tsx
@@ -1,31 +1,18 @@
-import { useState, ChangeEvent } from "react";
-
-
-export const WelcomeApp = ()=>{
-    const [query, setQuery] = useState('');
-  
-    const handleInputChange = (e:ChangeEvent<HTMLInputElement>) => {
-      setQuery(e.target.value);
-    };
-  
-    const handleSearch = () => {
-      // Lógica para enviar la petición al back con la búsqueda
-      console.log('Realizar búsqueda con:', query);
-    };
-
-    return(
+export const WelcomeApp = () => {  
+    return (
         <>
-        <h1>Confirmación de asistencia Boda Elisa y José</h1>
-        <div className="search-container">
-        <input
-            type="text"
-            placeholder="Buscar usuarios..."
-            value={query}
-            onChange={handleInputChange}
-        />
-        <button onClick={handleSearch}>Buscar</button>
-        </div>
-        <h5>Esperamos tu confirmacion</h5>
+            <h1>Confirmación de asistencia Boda</h1>
+            <h1>Elisa & José</h1>
+            <form>
+                <div className="search-container">
+                    <input
+                        type="text"
+                        placeholder="Nombre y apellido..."
+                        autoComplete="off"
+                    />
+                    <button>Buscar</button>
+                </div>
+            </form>            
         </>
-    )
+    );
 }
